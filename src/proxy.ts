fef0ce475e0a335cc568555c7180b55129eb0e71
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2025-12-05 17:26:13 +0600

    Renamed the middleware.ts file to proxy.ts as per Next.js new rules

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,14 +1,15 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export async function middleware(request: NextRequest) {
+// src/proxy.ts
+export async function proxy(request: NextRequest) {
   // For Firebase Auth, we'll handle authentication on the client side
   // The middleware will just allow all requests through
   // Authentication will be checked in the components/pages themselves
   return NextResponse.next();
 }
 
-// See "Matching Paths" below to learn more
+
 export const config = {
   matcher: '/profile',
-}; 
+};
